05e02c45c7801dad299d00ebe76b2c5b2e6ee577
KKami <amm1112094@gmail.com>
2024-04-01 21:02:56 +0700

    CSS Success

--- Client/src/screen/Payment/success/Success.jsx
+++ Client/src/screen/Payment/success/Success.jsx
@@ -1,5 +1,7 @@
+import { Container } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
+import './Success.css'
 
 function Success() {
     const [message, setMessage] = useState("");
@@ -13,7 +15,9 @@ function Success() {
 
     return (
         <>
+            <Container className="success-container">
             <h1>{message}</h1>
+            </Container>
         </>
     )
 }
